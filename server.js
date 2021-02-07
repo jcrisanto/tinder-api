70f151e552f79365e38c82dcc55f91d47aa52b0f
Marcus <marcushindsboel@gmail.com>
2021-02-07 16:23:28 +0100

    First commit

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const http = require("http");
 const express = require("express");
-
 const app = express();
 app.use(express.json());
 app.get("/hello", (req, res) => {
     res.status(200).json("Hello world");
 });
 const server = http.createServer(app);
-
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log("Server listening on port " + port);
 });
-
-
