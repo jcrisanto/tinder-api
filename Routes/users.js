05daf5254d8339953e66c1141da044bf7a0e4fcb
Marcus <marcushindsboel@gmail.com>
2021-02-08 16:04:05 +0100

    Added creation of deletion and update

--- Routes/users.js
+++ Routes/users.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require("express");
 const router = express.Router();
 const user = require("../Models/user.js");
+const { request } = require('http');
 
 let usersTable = [];
 loadChanges();
@@ -13,7 +14,7 @@ router.post('/register', function (req,res){
     let password = req.body.password;
     let newUser = new user(firstName, lastName, age, email, password)
     const foundUser = usersTable.find((u) => u.email === newUser.email);
-    if (foundUser) {
+    if  (foundUser) {
         res.status(400).send("Email already in use, try with a different email");
         return;
     }
@@ -31,6 +32,47 @@ router.get('/login', function (req, res) {
         return;
     }
     res.status(200).send("You are logged in now!");
+});
+
+router.get("/", function (req, res) {
+    const usersDTO = [...usersTable];
+    usersDTO.forEach((u) => u.password = "n/a");
+    res.status(200).send(usersTable);
+});
+
+router.get("/:id", function (req, res) {
+    const foundUser = usersTable.find((u) => u.id === req.params.id);
+    if  (foundUser) {
+        let userDTO = {...foundUser};
+        userDTO.password = "n/a";
+        res.status(200).send(userDTO);
+        return;
+    }
+    res.status(400).send("User was not found");
+});
+
+router.delete("/:id", function (req, res) {
+    const foundIndex = usersTable.findIndex((u) => u.id === req.params.id);
+    if (foundIndex === -1) {
+        res.status(400).send("User was not found");
+        return;
+    }
+    usersTable.splice(foundIndex, 1);
+    saveChanges();
+    res.status(200).send("User was deleted");
+});
+
+router.put("/", function (req, res) {
+    const userFromRequest = req.body;
+    let foundIndex = usersTable.findIndex((u) => u.id === userFromRequest.id);
+    if (foundIndex !== -1) {
+     usersTable[foundIndex] = userFromRequest;
+        saveChanges();
+        res.status(200).send("User was updated");
+        return;
+    }
+    res.status(400).send("User was not found");
+    console.log(req.body);
 });
 
 function saveChanges() {
