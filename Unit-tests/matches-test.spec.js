add3cd185b4d371906fbb1c5afc1cba4f118cec6
Marcus Hindsbøl <marcushindsboel@gmail.com>
2021-02-10 16:07:20 +0100

    Added DFD and small change to test

--- Unit-tests/matches-test.spec.js
+++ Unit-tests/matches-test.spec.js
@@ -15,6 +15,8 @@ test('When getting a random user, and sending an id of 2', () => {
     ]
     const userResponse = getRandomUser("2", users);
     expect(userResponse.id).toBe("1");
+    expect(userResponse.firstName).toBe("Marcus");
+    expect(userResponse.password).toBe("n/a");
   });
 
   test('When getting a random user, and we only have 1 user in the list', () => {
